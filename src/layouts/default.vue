b8beec6d20525ac596a2cbc33cdd7a58ba0ada2b
lazizbekshodmonov <lazizbekshodmonov001@gmail.com>
2023-12-18 22:28:32 +0500

    bugs fixed

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,6 @@
 <template>
-  <main
-    px-4 py-10
-    flex justify-center
-    text="center gray-700 dark:gray-200"
-  >
+  <main flex justify-center px-4 py-10 text="center gray-700 dark:gray-200">
     <RouterView />
-    
+    <the-footer />
   </main>
 </template>
